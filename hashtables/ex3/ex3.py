2ddd1594b2b401fe602e45c6baf3eddf8c0db2cf
Ro McKay Davies <50719703+120Davies@users.noreply.github.com>
2020-05-08 10:52:38 -0600

    Update ex3.py

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,23 @@
+import sys
+sys.path.append("../")
+from hashtables import (HashTable,
+                        hash_table_insert,
+                        hash_table_remove,
+                        hash_table_retrieve,
+                        hash_table_resize)
+
+# Building a dictionary works better.
 def intersection(arrays):
 
-    """
-    YOUR CODE HERE
-    """
+    hashtable = {}
+
+    for a in arrays:
+        for i in a:
+            if hashtable.get(i, None) is None:
+                hashtable[i] = 0
+            hashtable[i] += 1
+
+    result = [i for i, n in hashtable.items() if n == len(arrays)]
 
     return result
 
@@ -13,5 +28,5 @@ if __name__ == "__main__":
     arrays.append(list(range(1000000,2000000)) + [1,2,3])
     arrays.append(list(range(2000000,3000000)) + [1,2,3])
     arrays.append(list(range(3000000,4000000)) + [1,2,3])
-
+    
     print(intersection(arrays))
